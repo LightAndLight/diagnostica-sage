431c636174a5c39f1a3bfc1141bf7d95c0b5b399
Isaac Elliott <isaace71295@gmail.com>
2025-12-17 15:17:11 +1000

    Fix tests

--- test/Main.hs
+++ test/Main.hs
@@ -4,12 +4,14 @@ module Main where
 
 import Control.Applicative (many, (<|>))
 import Data.Bifunctor (first)
-import qualified Data.Text.Lazy as Lazy
+import qualified Data.ByteString.Lazy as LazyByteString
+import qualified Data.ByteString.Lazy.Char8 as LazyByteString.Char8
 import Test.Hspec
 import Text.Diagnostic (Config (colors), defaultConfig, render)
 import Text.Diagnostic.Sage (parseError)
 import Text.Parser.Char (char)
 import Text.Parser.Combinators (between, eof, sepBy)
+import Text.Parser.Sage.Instances ()
 import Text.Sage (parse)
 
 main :: IO ()
@@ -29,10 +31,10 @@ main =
 
             actual =
               first (render (defaultConfig{colors = Nothing}) "test" input . parseError) $
-                parse (sexpr <* eof) input
+                parse (sexpr <* eof) (LazyByteString.toStrict input)
             expected =
               Left $
-                Lazy.unlines
+                LazyByteString.Char8.unlines
                   [ "test:1:5: error: expected one of: ' ', '(', ')', 'x'"
                   , "  |"
                   , "1 | (x x"
@@ -45,10 +47,10 @@ main =
 
             actual =
               first (render (defaultConfig{colors = Nothing}) "test" input . parseError) $
-                parse (sexpr <* eof) input
+                parse (sexpr <* eof) (LazyByteString.toStrict input)
             expected =
               Left $
-                Lazy.unlines
+                LazyByteString.Char8.unlines
                   [ "test:1:2: error: expected end of file"
                   , "  |"
                   , "1 | xy"
